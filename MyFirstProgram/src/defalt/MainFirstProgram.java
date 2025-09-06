a3955d43778582bde1a443ae4a5b18fb5e443c11
Gabriel H <garolim1010@gmail.com>
2025-09-06 20:23:43 -0300

    MiniMudança.java
    - Troca de das respostas, e área da pergunta.

--- MyFirstProgram/src/defalt/MainFirstProgram.java
+++ MyFirstProgram/src/defalt/MainFirstProgram.java
@@ -7,18 +7,19 @@ public class MainFirstProgram {
 	public static void main(String[] args) {
 
 			try(Scanner scanner = new Scanner(System.in)) {
-				System.out.print("Digite sua idade: ");
+				System.out.print("Digite sua idade a seguir: ");
 			int infoIdade = scanner.nextInt();
 			
 			if(infoIdade>=18) {
-				System.out.println("Você é um adulto!");
+				System.out.println("Você é um adulto.");
 			}
 			else if(infoIdade>=13) {
 				System.out.println("Você é um adolescente.");
 			}
 			else {
-				System.out.println("Você não é adulto!");
+				System.out.println("Você é uma criança.);
 			}
 		}
 	}
+
 }                                                                                                  
