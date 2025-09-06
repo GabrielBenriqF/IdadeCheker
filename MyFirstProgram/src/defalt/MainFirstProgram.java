a35ae36b673fc78c08e9fb19169f5125ce6a392a
Gabriel H <garolim1010@gmail.com>
2025-09-06 20:58:41 -0300

    MiniTerceiraMudança.java
    - Adicionado a versão "Você é idoso", e a "finalização" ao terminar o programa.

--- MyFirstProgram/src/defalt/MainFirstProgram.java
+++ MyFirstProgram/src/defalt/MainFirstProgram.java
@@ -6,21 +6,25 @@ public class MainFirstProgram {
 
 	public static void main(String[] args) {
 
-			try(Scanner scanner = new Scanner(System.in)) {
-				System.out.print("Digite sua idade a seguir: ");
-			int infoIdade = scanner.nextInt();
-			
-			if(infoIdade>=18) {
-				System.out.println("Você é um adulto.");
-			}
-			else if(infoIdade>=13) {
-				System.out.println("Você é um adolescente.");
-			}
-			else {
-				System.out.println("Você é uma criança.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite sua idade a seguir: ");
+            int infoIdade = scanner.nextInt();
+
+            if (infoIdade >= 65) {
+                System.out.println("Você é um idoso.");
+            } else if (infoIdade >= 18) {
+                System.out.println("Você é um adulto.");
+            } else if (infoIdade >= 13) {
+                System.out.println("Você é um adolescente.");
+            } else if (infoIdade < 13) {
+                System.out.println("Você é uma criança.");
+            }
+			{
+                System.out.println("Encerrando o programa...");
 			}
 		}
 	}
 
 }                                                                                                  
+
 
